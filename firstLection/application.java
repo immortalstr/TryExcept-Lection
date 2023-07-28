2e2ac7dd60e058ee8797350e5fadb07e5a014f14
immortalstr <immortalstr1990@mail.ru>
2023-07-29 00:21:37 +0300

    add code in application

--- firstLection/application.java
+++ firstLection/application.java
@@ -9,21 +9,33 @@ public class application {
 
     public static void main(String[] args) {
         // System.out.println(GetFileSize(new File("123")));
-        System.out.println(divide(-10, 10));
-
+        // System.out.println(divide(-10, 0));
+        a(); 
     }
 
-    public static int divide(int a, int b) {
-        if (b == 0) {
-            return -1;
-        }
-        return a / b;
+    public static void a() {
+        b();
     }
 
-    public static long GetFileSize(File file) {
-        if (!file.exists()) {
-            return -1L;
-        }
-        return file.length();
+    public static void b() {
+        c();
     }
+
+    public static void c() {
+        int[] ints = new int[10];
+        System.out.println(ints[1000]);
+    }
+    // public static int divide(int a, int b) {
+    // // if (b == 0) {
+    // // return -1;
+    // // }
+    // return a / b;
+    // }
+
+    // public static long GetFileSize(File file) {
+    // if (!file.exists()) {
+    // return -1L;
+    // }
+    // return file.length();
+    // }
 }
