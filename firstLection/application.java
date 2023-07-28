340ec4546c4d5c0a4ada39259f08d1d9e310a7bc
immortalstr <immortalstr1990@mail.ru>
2023-07-29 00:41:51 +0300

    Add Folder Seminar

--- firstLection/application.java
+++ firstLection/application.java
@@ -10,21 +10,30 @@ public class application {
     public static void main(String[] args) {
         // System.out.println(GetFileSize(new File("123")));
         // System.out.println(divide(-10, 0));
-        a(); 
+        // a();
+        System.out.println(divide(10, 0));
     }
 
-    public static void a() {
-        b();
+    public static int divide(int a1, int a2) {
+        if (a2 == 0) {
+            throw new RuntimeException("Divide by zero not permited");
+        }
+        return a1 / a2;
+
     }
 
-    public static void b() {
-        c();
-    }
+    // public static void a() {
+    // b();
+    // }
 
-    public static void c() {
-        int[] ints = new int[10];
-        System.out.println(ints[1000]);
-    }
+    // public static void b() {
+    // c();
+    // }
+
+    // public static void c() {
+    // int[] ints = new int[10];
+    // System.out.println(ints[1000]);
+    // }
     // public static int divide(int a, int b) {
     // // if (b == 0) {
     // // return -1;
