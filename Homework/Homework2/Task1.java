4d6dc8a7cd055542b4752af1691f39a16979d21a
immortalstr <immortalstr1990@mail.ru>
2023-07-31 01:26:11 +0300

    finish Task2

--- Homework/Homework2/Task1.java
+++ Homework/Homework2/Task1.java
@@ -32,9 +32,8 @@ public class Task1 {
         }
     }
 
-
     public static void main(String[] args) {
 
-System.out.println(input());
+        System.out.println(input());
     }
 }
--- Homework/Homework2/Task2.java
+++ Homework/Homework2/Task2.java
@@ -1,5 +1,19 @@
 package Homework.Homework2;
 
 public class Task2 {
-    
+    public static void main(String[] args) {
+        int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        try {
+            int d = 1;
+            double catchedRes1 = intArray[9] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e.getMessage());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Catching exception: " + e.getMessage());
+
+        }
+
+    }
 }
