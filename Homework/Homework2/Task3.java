f7942e9843a0a128fcae00de92c3f410bef05f84
immortalstr <immortalstr1990@mail.ru>
2023-07-31 22:01:39 +0300

    Add and finish Task3 in Sem2

--- Homework/Homework2/Task3.java
+++ Homework/Homework2/Task3.java
@@ -4,23 +4,26 @@ import java.io.FileNotFoundException;
 
 public class Task3 {
     public static void main(String[] args) throws Exception {
-   try {
-       int a = 90;
-       int b = 3;
-       System.out.println(a / b);
-       printSum(23, 234);
-       int[] abc = { 1, 2 };
-       abc[3] = 9;
-   } catch (Throwable ex) {
-       System.out.println("Что-то пошло не так...");
-   } catch (NullPointerException ex) {
-       System.out.println("Указатель не может указывать на null!");
-   } catch (IndexOutOfBoundsException ex) {
-       System.out.println("Массив выходит за пределы своего размера!");
-   }
-}
-public static void printSum(Integer a, Integer b) throws FileNotFoundException {
-   System.out.println(a + b);
-}
+        try {
+            int a = 90;
+            int b = 1;
+            System.out.println(a / b);
+            printSum(null, null);
+            int[] abc = {};
+            abc[3] = 9;
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundExceptio");
+        } catch (NullPointerException e) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero");
+        }
+        
+    }
 
-}`
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+}
