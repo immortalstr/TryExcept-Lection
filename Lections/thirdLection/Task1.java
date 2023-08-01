25a8c0f959bf430aeba207f7792496b3ecaad19e
immortalstr <immortalstr1990@mail.ru>
2023-08-01 23:35:42 +0300

    Create folder 'thirdLection and add files in

---
+++ Lections/thirdLection/Task1.java
@@ -0,0 +1,29 @@
+// package Lections.thirdLection;
+
+// import java.io.BufferedReader;
+// import java.io.BufferedWriter;
+// import java.io.File;
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Path;
+
+// /**
+//  * Task1
+//  */
+// public class Task1 {
+
+// public void rwline (Path pathRead, Path pathWrite) throws IOException{
+// BufferedReader in = null;
+// BufferedWriter out = null;
+
+// try
+// (in = Files.newBufferedReader(pathRead);
+// out = Files.newBufferedWriter(pathWrite);)
+// {
+//     out.write(in.readLine());
+// }
+//     catch(IOException e){
+//     System.out.println();
+// }
+// }
+// }
---
+++ Lections/thirdLection/Task2.java
@@ -0,0 +1,21 @@
+package Lections.thirdLection;
+
+// Создайте метод doSomething(), который может быть источником одного из типов checked 
+// exceptions (тело самого метода прописывать не обязательно). Вызовите этот метод из main и 
+// обработайте в нем исключение, которое вызвал метод doSomething()
+
+public class Task2 {
+    
+
+public static void doSomething() throws Exception{
+throw new Exception("Error!!!");
+
+}
+
+public static void main(String[] args) {
+    try{
+    doSomething();
+    }catch(Exception e){e.printStackTrace();}
+}
+
+}
